113283e970cdce24b8924c75271249660522a582
jesus <jesusgilcabezas@gmail.com>
2026-09-01 23:01:01 +0200

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // URL canónica fija necesaria para que @astrojs/sitemap no falle en el primer build
   site: 'https://carreracinturonverdecordoba.vercel.app',
   integrations: [
-    tailwind()
+    tailwind(),
+    sitemap()
   ],
   output: 'static'
 });
