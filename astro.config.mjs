35e849528e711fc2c6f0193fbc1ef037a9f1f58f
jesus <jesusgilcabezas@gmail.com>
2026-09-01 22:42:14 +0200

    fixs site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://carrera-cinturon-verde-cordoba.vercel.app',
+  site: 'https://carreracinturonverdecordoba.vercel.app',
   integrations: [
     tailwind(),
     sitemap()
