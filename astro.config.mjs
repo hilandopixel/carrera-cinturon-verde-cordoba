5eef28f46766dc14bf7db53dee66f5ba5f7d02eb
jesus <jesusgilcabezas@gmail.com>
2026-09-01 23:36:06 +0200

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://carreracinturonverdecordoba.vercel.app',
-  output: 'hybrid', // Permite renderizado estático + funciones API
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [tailwind()]
 });
