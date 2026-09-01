00d14c78cb1c2ee159d0d8277773df6013fd5995
jesus <jesusgilcabezas@gmail.com>
2026-09-01 23:00:17 +0200

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // URL canónica fija necesaria para que @astrojs/sitemap no falle en el primer build
